1d7a08d6596586260a77bce3693e25e44c6cb99a
Arek Cios <arek123113@gmail.com>
2020-10-15 19:36:40 +0200

    Add Typescript and getToken

---
+++ pages/lib/getToken.js
@@ -0,0 +1,16 @@
+const client_id = 'ea5147c9c40b425fa19cdddc4d055e99'
+const redirect_uri = 'http://localhost:3000';
+
+function getToken() {
+    const base_url = 'https://accounts.spotify.com/authorize';
+    let url = base_url +
+    '?client_id=' +
+    client_id +
+    '&response_type=code&redirect_uri=' +
+    redirect_uri +
+    '&scope=user-top-read%20user-read-recently-played%20user-read-currently-playing';
+
+    return url;
+}
+
+export default getToken;
