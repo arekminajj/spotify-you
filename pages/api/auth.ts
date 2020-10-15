887b188835e857046ed8239a2b4e13c6456daaba
Arek Cios <arek123113@gmail.com>
2020-10-15 21:06:55 +0200

    Move getAccessToken to a different location

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,28 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
-
-async function getAccessToken(code:string) {
-  const client_id = 'ea5147c9c40b425fa19cdddc4d055e99';
-  const client_secret = process.env.client_secret;
-  const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/x-www-form-urlencoded', 
-                'Authorization' : 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
-            },
-            body: new URLSearchParams({
-              'grant_type': 'authorization_code',
-              'code': code,
-              'redirect_uri': 'http://localhost:3000/api/auth'
-            })
-        });
-
-        const data = await result.json();
-        console.log(data)
-        return data.access_token;
-}
+import getAccessToken from './lib/getAccessToken'
 
 export default (req:NextApiRequest, res:NextApiResponse) => {
-  
   const {
     query: { code },
   } = req
---
+++ pages/api/lib/getAccessToken.js
@@ -0,0 +1,23 @@
+async function getAccessToken(code) {
+    const client_id = 'ea5147c9c40b425fa19cdddc4d055e99';
+    const client_secret = process.env.client_secret;
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+              method: 'POST',
+              headers: {
+                  'Content-Type' : 'application/x-www-form-urlencoded', 
+                  'Authorization' : 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+              },
+              body: new URLSearchParams({
+                'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': 'http://localhost:3000/api/auth'
+              })
+          });
+  
+          const data = await result.json();
+          console.log(data)
+          return data.access_token;
+  }
+
+  export default getAccessToken;
+  
