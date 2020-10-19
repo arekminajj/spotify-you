3761c6d048f112984ce34ff921024f0ab48040d5
Arek Cios <arek123113@gmail.com>
2020-10-19 16:04:11 +0200

    Style the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,29 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Container, Jumbotron, Button, Navbar, Image } from "react-bootstrap";
+import getAuthCodeUrl from './lib/getAuthCodeUrl'
 
 export default function Home() {
+  const url = getAuthCodeUrl();
   return (
-    <div></div>
-  )
+    <Container>
+      <Navbar>
+        <Navbar.Brand href="#home">Spotify-You</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <p>Hello!</p>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Jumbotron>
+        <h1>Spotify-you</h1>
+        <p>
+          Check out your listening stats on spotify!
+        </p>
+        <p>
+          <Button href={url} variant="primary">Click here!</Button>
+        </p>
+      </Jumbotron>
+    </Container>
+  );
 }
