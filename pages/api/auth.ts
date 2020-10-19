d6df47d2987e97ad66ff941ce210d090dd0f1d49
Arek Cios <arek123113@gmail.com>
2020-10-19 10:49:42 +0200

    Add user page and fix redirect bug

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -5,31 +5,32 @@ import getRefreshedAccessToken from './lib/getRefreshedAccessToken';
 import getUsersTopTracks from './lib/getUsersTopTracks';
 import getUsersTopArtists from './lib/getUsersTopArtists';
 import getUsersCurrentlyPlayingTrack from './lib/getUsersCurrentlyPlayingTrack';
-import getUsersRecentlyPlayedTracks from './lib/getUsersRecentlyPlayedTracks'
+import getUsersRecentlyPlayedTracks from './lib/getUsersRecentlyPlayedTracks';
 
 export default async (req:NextApiRequest, res:NextApiResponse) => {
   const {
     query: { code },
   } = req
 
-  //TESTING HERE IF IT WORKS FOR NOW
-  var data = await getAccessToken(code.toString());
-  var token = data.access_token;
-  //console.log(token);
-  token = await getRefreshedAccessToken(data.refresh_token);
-  //var user = await getAuthorizedUser(token);
-  //console.log(user);
-  //var topArtists = await getUsersTopArtists(token, "50", "long_term")
-  //console.log(topArtists)
-  //token = await getRefreshedAccessToken(data.refresh_token);
-  //var topTracks = await getUsersTopTracks(token, "50", "long_term");
-  //console.log(topTracks);
-  //var currentlyPlaying = await getUsersCurrentlyPlayingTrack(token);
-  //console.log(currentlyPlaying.item.artists[0].name);
+  const token_Data = await getAccessToken(code.toString());
+  let token = await token_Data.access_token;
+  token = await getRefreshedAccessToken(token_Data.refresh_token);
 
-  var recentlyPlayed = await getUsersRecentlyPlayedTracks(token, "50");
-  console.log(recentlyPlayed.items[0].track);
+  const user = await getAuthorizedUser(token);
+  const topArtists = await getUsersTopArtists(token, "50", "long_term")
+  const topTracks = await getUsersTopTracks(token, "50", "long_term");
+  const currentlyPlaying = await getUsersCurrentlyPlayingTrack(token); //FOR NOW USER HAVE TO PLAY SMTH SO IT WONT CRASH
+  const recentlyPlayed = await getUsersRecentlyPlayedTracks(token, "50");
 
   res.statusCode = 200
-  res.json({ status: "Success!" })
+  res.json(
+    { status: "Success!",
+      data: {
+        user: user,
+        topArtists: topArtists,
+        topTracks: topTracks,
+        currentlyPlaying: currentlyPlaying,
+        recentlyPlayed: recentlyPlayed
+      }
+  })
 }
